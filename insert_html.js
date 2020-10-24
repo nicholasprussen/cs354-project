8dd6167e17899da075922a890fd697b8acd635db
nicholasprussen <nicholasprussen@u.boisestate.edu>
2020-10-24 14:48:51 -0500

    cleaned up files. Added basic menu and iframe changing functionality

--- insert_html.js
+++ insert_html.js
@@ -1,21 +1,9 @@
-// var div = document.createElement('div');
-// div.id = "fuck";
-// //div.innerHTML = "<h1>fuck</h1>";
-// //$('fuck').load(chrome.extension.getURL('test.html'));
-// // $.get(chrome.extension.getURL('test.html'), function(html){
-// //      var $html = $(html);
-// //      div.innerHTML = $html.find('div').text();
-// //  });
-// div.height = "500px";
-// div.width = "500px";
-// div.style.background = "#000000";
-// div.style.zIndex = 99999999;
-// div.innerText = "fuck";
-//$("#fuck").load(chrome.extension.getURL("test.html"));
+//Iframe that holds the embedded video
+const myIframe = '<iframe id="iframe-video-container" style="position: fixed; right: 15px; top: 130px;" width="640px" height="360p"  src="https://www.youtube.com/embed/aMlMcyO8Oag" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
-$testURL = chrome.extension.getURL('test.html');
-const myIframe = '<iframe height="100%" width="100%" style="position: sticky" src="chrome-extension://ikcnkbebhgbbhpkhjacplbgcmgdhccfb/test.html"></iframe>';
+//div to hold the menu and iframe
 let div = document.createElement('div');
+div.id = "extension-container";
 div.style.zIndex = 2147483647;
 div.style.position = "absolute";
 div.style.right = "15px";
@@ -23,8 +11,33 @@ div.style.top = "65px";
 div.style.width = "640px";
 div.style.height = "360px";
 div.style.textAlign = "right";
+//insert the iframe
 div.innerHTML = myIframe;
 
+//create the menu and append to main div
+let iframeMenu = document.createElement('div');
+iframeMenu.id = "iframe-menu";
+iframeMenu.innerHTML =  '<div class="row" style="position: fixed; top: 80px; right: 15px; width:640px; height:50px; background: #000000;">' +
+                            '<div class="column" style="background: #000000; height: 100%; width:25%; float: left;"></div>' + 
+                            '<div class="column" style="background: #00ff00; height: 100%; width:25%; float: left;"></div>' +
+                            '<div class="column" style="background: #ff0000; height: 100%; width:25%; float: left;">' +
+                                '<button id="youtube-add-link" style="width:100%; height:100%">YouTube</button>' +
+                            '</div>' +
+                            '<div class="column" style="background: #0000ff; height: 100%; width:25%; float: left;"></div>' +
+                        '</div>';
+//append menu to div
+div.prepend(iframeMenu);
+
+//append entire div to website
 $("body").append(div);
 
-//document.body.appendChild(div);
+//check to make sure button exists before adding event listener
+if(document.getElementById('youtube-add-link')){
+    document.getElementById('youtube-add-link').addEventListener("click", changeEmbededVideo);
+}
+
+//this is what happens when the youtube button is clicked
+function changeEmbededVideo() {
+    changingIframe = document.getElementById('iframe-video-container');
+    changingIframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+}
