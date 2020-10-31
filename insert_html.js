5409c690b328cfc9c33b7c6f349819a709f79c53
nicholasprussen <nicholasprussen@u.boisestate.edu>
2020-10-31 13:26:35 -0600

    added a hide content button. temp way to access both jquerys for now

--- insert_html.js
+++ insert_html.js
@@ -1,5 +1,5 @@
 //Iframe that holds the embedded video
-const myIframe = '<iframe id="iframe-video-container" style="position: fixed; right: 15px; top: 130px;" width="640px" height="360p"  src="https://www.youtube.com/embed/aMlMcyO8Oag" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+var myIframe = '<iframe id="iframe-video-container" style="position: fixed; right: 15px; top: 130px;" width="640px" height="360p"  src="https://www.youtube.com/embed/aMlMcyO8Oag" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
 //div to hold the menu and iframe
 let div = document.createElement('div');
@@ -18,6 +18,7 @@ div.innerHTML = myIframe;
 let iframeMenu = document.createElement('div');
 iframeMenu.id = "iframe-menu";
 iframeMenu.innerHTML =  '<div class="row" style="position: fixed; top: 80px; right: 15px; width:640px; height:50px; background: #000000;">' +
+                            '<div id="youtube-search-bar" style="position: absolute; background: grey; display: none; width: 100%; height: 100px"' +
                             '<div class="column" style="background: #000000; height: 100%; width:25%; float: left;"></div>' + 
                             '<div class="column" style="background: #00ff00; height: 100%; width:25%; float: left;"></div>' +
                             '<div class="column" style="background: #ff0000; height: 100%; width:25%; float: left;">' +
@@ -25,8 +26,15 @@ iframeMenu.innerHTML =  '<div class="row" style="position: fixed; top: 80px; rig
                             '</div>' +
                             '<div class="column" style="background: #0000ff; height: 100%; width:25%; float: left;"></div>' +
                         '</div>';
+
+let hideIframe = document.createElement('div');
+hideIframe.id = "hideIframeButton";
+hideIframe.innerHTML = '<div id="hide-content" class="row" style="position: fixed; top: 490px; right: 15px; width:640px; height:50px; background: #000000;">' +
+                            '<button id="hide-content-button" style="width: 100%; height: 100%; background: grey;"> Hide Content </button>"' +
+                       '</div>'
 //append menu to div
 div.prepend(iframeMenu);
+div.append(hideIframe);
 
 //append entire div to website
 $("body").append(div);
@@ -36,8 +44,25 @@ if(document.getElementById('youtube-add-link')){
     document.getElementById('youtube-add-link').addEventListener("click", changeEmbededVideo);
 }
 
+document.getElementById("hide-content-button").addEventListener("click", hideContent);
+
 //this is what happens when the youtube button is clicked
 function changeEmbededVideo() {
-    changingIframe = document.getElementById('iframe-video-container');
-    changingIframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+    //let extCont = document.getElementById("extension-container");
+    //extCont.top = "100px";
+    //let menuDiv = document.getElementById("iframe-menu");
+    //menuDiv.height = "100px"
+    div = document.getElementById("iframe-video-container");
+    div.style.top="180px";
+    youtube = document.getElementById("youtube-search-bar");
+    youtube.style.display = "block";
+    hideContent = document.getElementById("hide-content");
+    hideContent.style.top = "540px";
+    //invisibleDiv = document.getElementById('youtube-search-bar');
+    //changingIframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+    //invisibleDiv.style.display = "block";
+}
+
+function hideContent() {
+    document.getElementById("extension-container").style.display = "none";
 }
