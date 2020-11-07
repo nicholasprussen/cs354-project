9f2e2ed29bf172cc182bf0db606e5e7412a9b93a
nicholasprussen <59841031+nicholasprussen@users.noreply.github.com>
2020-11-07 13:49:40 -0700

    added draggability and resizability

--- YouTube/youtube.js
+++ YouTube/youtube.js
@@ -5,11 +5,21 @@
 //Create main container div
 div = createContainerDiv();
 
+let iFrameDiv = document.createElement("div");
+iFrameDiv.id = "iframe-object";
+iFrameDiv.style.height = "85%";
+iFrameDiv.style.width = "100%";
+iFrameDiv.style.display = "none";
+//Iframe that holds the embedded video
+var myIframe = '<iframe id="iframe-video-container" style="position: relative; display: none;" width="100%" height="100%"  src="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+iFrameDiv.innerHTML = myIframe;
+
 //create the menu and append to main div
 let iframeMenu = document.createElement('div');
 iframeMenu.id = "iframe-menu";
+iframeMenu.style.height = "50%";
 iframeMenu.innerHTML =  
-                        '<div id="youtube-search-bar" style="position: fixed; background: black; top: 80px; display: none; width: 640px; height: 50px"' +
+                        '<div id="youtube-search-bar" style="position: relative; background: black; display: none; width: 100%; height: 100%"' +
                             '<div id="form-container" style="padding-left: 10px; height: 100%; width: 100%">' +
                                 '<form">' +
                                     '<input style="float: left" id="vidLink-value" type="text" name="vidLink" />' +
@@ -18,19 +28,18 @@ iframeMenu.innerHTML =
                                 '</form>' +
                             '</div>' +
                         '</div>' +
-                        '<div id="social-menu" class="row" style="position: fixed; top: 80px; right: 15px; width:640px; height:50px; background: #000000;">' +
-                            '<div class="column" style="background: #000000; height: 100%; width:25%; float: left;"></div>' + 
-                            '<div class="column" style="background: #00ff00; height: 100%; width:25%; float: left;"></div>' +
-                            '<div class="column" style="background: #ff0000; height: 100%; width:25%; float: left;">' +
-                                '<button id="youtube-add-link" style="width:100%; height:100%">YouTube</button>' +
-                            '</div>' +
-                            '<div class="column" style="background: #0000ff; height: 100%; width:25%; float: left;"></div>' +
+                        '<div id="social-menu" style="text-align: center; position: relative; width:100%; height:100%; background: #000000;">' +
+                            '<button id="news" style="width:25%; height: 100%; background-color: green; border-right: none; display: inline-block;">News</button>' +
+                            '<button id="youtube-add-link" style="width:25%; height: 100%; background-color: red; border-right: none; display: inline-block;">YouTube</button>' +
+                            '<button id="twitch" style="width:25%; height: 100%; background-color: purple; border-right: none; display: inline-block;">Twitch</button>' +
+                            '<button id="reddit" style="width:25%; height: 100%; background-color: red; display: inline-block;">Reddit</button>' +
                         '</div>';
 
 //container for hide elements buttons
 let hideIframe = document.createElement('div');
 hideIframe.id = "hideIframeButton";
-hideIframe.innerHTML = '<div id="hide-content" class="row" style="text-align: center; position: fixed; top: 130px; right: 15px; width:640px; height:50px; background: #000000;">' +
+hideIframe.style.height = "50%";
+hideIframe.innerHTML = '<div id="hide-content" class="row" style="text-align: center; position: relative; width:100%; height:100%; background: #000000;">' +
                             '<div id="everything-button-container" class="column" style="height: 100%; width:100%; float: left;">' +
                                 '<button id="hide-everything-button" style="width: 100%; height: 100%; background: grey;"> Hide Everything </button> </div>' +
                             '<div id="content-button-container" class="column" style="height: 100%; width:50%; float: left; display: none; ">' +
@@ -40,10 +49,48 @@ hideIframe.innerHTML = '<div id="hide-content" class="row" style="text-align: ce
 
 //append menu and hide buttons to div
 div.prepend(iframeMenu);
+div.append(iFrameDiv);
 div.append(hideIframe);
 
+
+
+let draggableDiv = document.createElement('div');
+draggableDiv.id = "draggable-container";
+draggableDiv.style.height = "125px";
+draggableDiv.style.width = "640px";
+draggableDiv.style.position = "fixed";
+draggableDiv.style.display = "block";
+draggableDiv.style.background = "#3489eb";
+draggableDiv.style.top = "50px";
+draggableDiv.style.right = "10px";
+draggableDiv.className = "draggable";
+draggableDiv.style.textAlign = "center";
+draggableDiv.style.zIndex = "2147483647";
+
+draggableDiv.append(div);
+
 //append entire div to website
-$("body").append(div);
+$("body").append(draggableDiv);
+
+document.getElementsByTagName("body")[0].style.height = "100%";
+document.getElementsByTagName("html")[0].style.height = "100%";
+
+
+$(function() {
+    $(".draggable").draggable();
+});
+
+$(function (){
+    $(".draggable").resizable({
+       aspectRatio: 4/3, 
+       minHeight: 125,
+       minWidth: 640
+    });
+
+    $(".draggable")
+});
+
+
 
 //add event listeners to all the buttons embedded
 document.getElementById('youtube-add-link').addEventListener("click", enterInsertVideoMode);
@@ -61,8 +108,6 @@ document.getElementById("goBackButton").addEventListener("click", goBackFromYout
 //This is called at the beginning, creates main container div to be injected
 function createContainerDiv() {
 
-    //Iframe that holds the embedded video
-    var myIframe = '<iframe id="iframe-video-container" style="position: fixed; right: 15px; top: 130px; display: none;" width="640px" height="360p"  src="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
 
     //create main container div
@@ -71,10 +116,10 @@ function createContainerDiv() {
 
     //set div properties
     retDiv.id = "extension-container";
-    retDiv.style.cssText = "z-index: 2147483647; position: absolute; right: 15px; top: 65px; width: 640px; height: 360px; text-align: right";
+    retDiv.style.cssText = "z-index: 2147483647; position: relative; top:10%; width: 100%; height: 90%;";
 
     //insert the iframe
-    retDiv.innerHTML = myIframe;
+    retDiv.innerHTML = "<div></div>";
     
     return retDiv;
 }
@@ -93,7 +138,7 @@ function submitNewYoutubeLink() {
     document.getElementById("iframe-video-container").style.display = "block";
 
     //move hide content button down
-    document.getElementById("hide-content").style.top = "490px";
+    //document.getElementById("hide-content").style.top = "490px";
     document.getElementById("everything-button-container").style.width = "50%";
     document.getElementById("content-button-container").style.display = "block";
 
@@ -102,14 +147,27 @@ function submitNewYoutubeLink() {
 
     //unhide menu
     document.getElementById("social-menu").style.display = "block";
+
+    //extend the draggable box
+    document.getElementById("draggable-container").style.height = "480px";
+    document.getElementById("draggable-container").style.width = "640px";
+
+    //resize everyone's height
+    document.getElementById("extension-container").style.top = "4%";
+    document.getElementById("iframe-menu").style.height = "9%";
+    document.getElementById("hideIframeButton").style.height = "11%";
+    document.getElementById("iframe-object").style.display = "block";
+
+    
 }
 
 //go back from the submission menu
 function goBackFromYoutube(){
-    document.getElementById("iframe-video-container").style.top = "130px";
     document.getElementById("youtube-search-bar").style.display = "none";
-    document.getElementById("hide-content").style.top = "410px";
     document.getElementById("social-menu").style.display = "block";
+
+    //shrink draggable box
+    document.getElementById("draggable-container").style.height = "125px";
 }
 
 //this is what happens when the youtube button is clicked
@@ -131,6 +189,7 @@ function changeEmbededVideo(link) {
 //hide all elements on the page
 //TODO add small button to bring it back possibly in menu
 function hideEverything() {
+    document.getElementById("draggable-container").style.display = "none";
     document.getElementById("extension-container").style.display = "none";
 }
 
@@ -142,10 +201,20 @@ function hideContent(){
     document.getElementById("iframe-video-container").style.display = "none";
 
     //move buttons back up and hide content button
-    document.getElementById("hide-content").style.top = "130px";
     document.getElementById("everything-button-container").style.width = "100%";
     document.getElementById("content-button-container").style.display = "none";
 
     //remove src to stop video playback
     document.getElementById("iframe-video-container").src = "";
+
+    //shrink draggable back down
+    document.getElementById("draggable-container").style.height = "125px";
+
+    //resize everyone's height
+    document.getElementById("extension-container").style.top = "10%";
+    document.getElementById("iframe-menu").style.height = "50%";
+    document.getElementById("hideIframeButton").style.height = "50%";
+    document.getElementById("iframe-object").style.display = "none";
+
+    document.getElementById("draggable-container").style.width = "640px";
 }
