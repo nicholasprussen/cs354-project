e4a902fd9c0512632a76d7060e4b9a5dc334fcb8
ofilson <SuperScorer911@gmail.com>
2020-11-08 14:07:16 -0700

    fetch?

--- options/options.js
+++ options/options.js
@@ -22,7 +22,20 @@ function constructOptions(sites)
       } else {
       sitesArray[item].hidden = 0;
       }
-      //console.log(sitesArray);
+      // fetch(chrome.runtime.getURL("sites.json"), {
+      //   method: 'PUT',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: sites,
+      // })
+      // .then(response => response.json())
+      // .then(data => {
+      //   console.log('Success:', data);
+      // })
+      // .catch((error) => {
+      //   console.error('Error:', error);
+      // });
     });
     try {
       page = document.getElementById('site_checkboxes');
